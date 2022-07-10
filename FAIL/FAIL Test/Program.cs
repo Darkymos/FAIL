1fa4802a7d0e09a66c7f7a93f57ceebb407007db
Grandiras <Michael.Helmut.Mueller@gmx.de>
2022-07-10 20:44:44 +0200

    proofed multi file capability

--- FAIL/FAIL Test/Program.cs
+++ FAIL/FAIL Test/Program.cs
@@ -1,4 +1,6 @@
 ﻿using FAIL.Language_Integration;
 
-var interpreter = new Interpreter(LogLevel.Debug, @"C:\Users\Micha\Desktop\Darkymos\FAIL\FAIL\FAIL Test\Test.fail");
+var interpreter = new Interpreter(LogLevel.Debug, 
+                                  @"C:\Users\Micha\Desktop\Darkymos\FAIL\FAIL\FAIL Test\Maths.fail",
+                                  @"C:\Users\Micha\Desktop\Darkymos\FAIL\FAIL\FAIL Test\Test.fail");
 Console.WriteLine();
